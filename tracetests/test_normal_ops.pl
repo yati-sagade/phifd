d71b72030ccd3b0cc1818959f8f2b47adeca70c2
Yati Sagade <ysagade@fastmail.nl>
2018-01-20 20:46:46 +0100

    Tests: Normal operations, skeleton without the actual checks
    This spawns 3 nodes, waits for some time and then stops the nodes. The
    actual checking would be done by analysing the generated logs.
    
    The script is a "modulino", as described here[1], just to keep the
    script tidy.
    
    [1]: http://www.onlamp.com/pub/a/onlamp/2007/04/12/five-ways-to-improve-your-perl-programming.html

--- tracetests/test_normal_ops.pl
+++ tracetests/test_normal_ops.pl
@@ -1,79 +1,121 @@
+package Local::PhiFD::TestNormalOps;
+
 use strict;
 use warnings;
 use feature qw(say);
 use Getopt::Long;
 use List::MoreUtils qw(all);
 use Scalar::Util qw(looks_like_number);
+use IPC::Run qw(run timeout start harness);
+use File::Spec;
+use File::Path qw(make_path remove_tree);
 
-use constant {
-    NUM_PROCS => 3,
-};
+use constant { TEST_NAME => 'test_normal_ops', };
 
-my %opts;
-GetOptions(\%opts,
-    'num_procs=i',
-    'bind_host=s',
-    'ping_interval=i',
-    'test_runtime=i',
-) or die 'Invalid options found';
+sub new {
+    my ( $class, %opts ) = @_;
 
-my $num_procs = $opts{num_procs} || NUM_PROCS;
+    my $self = {};
 
-my $bind_host = $opts{bind_host} || '127.0.0.1';
-die 'bind_host must name an ipv4 address'
-    unless valid_ipv4_addr( $bind_host );
+    $self->{phifd} =
+         $opts{phi_exec}
+      || $ENV{PHIFD}
+      || die 'Set PHIFD to the path to the FD binary.';
 
-my $ping_interval = $opts{ping_interval} || 1; # seconds
-die 'ping_interval must be a positive, integral number of seconds'
-    unless $ping_interval > 0;
+    $self->{test_runtime} = $opts{test_runtime} || 30;    # seconds
+    die 'test_runtime must be a positive, integral number of seconds'
+      unless $self->{test_runtime} > 0;
 
-my $test_runtime = $opts{test_runtime} || 60; # seconds
-die 'test_runtime must be a positive, integral number of seconds'
-    unless $test_runtime > 0;
+    $self->{ping_interval} = $opts{ping_interval} || 1;    # seconds
+    die 'ping_interval must be a positive, integral number of seconds'
+      unless $self->{ping_interval} > 0;
 
-my @pids;
-my $base_port = 12345;
-my $start = time;
-for my $procnum ( 1..NUM_PROCS ) {
-    my $port = $base_port + $procnum - 1;
-    my $pid = fork();
-    if ( $pid ) {
-        say "monitor: spawned $pid";
-        push @pids, $pid;
-        next;
-    }
-    my $bind_addr = $bind_host.':'.$port;
-    my @cmd = ( '../target/debug/phifd',
-                '-t', $ping_interval,
-                '-a', $bind_addr );
+    $self->{log_root} = $opts{log_root} || 'logs/';
+    $self->{test_name} = $opts{test_name};
 
-    if ( $procnum > 1 ) {
-        push @cmd, '-i', $bind_host.':'.$base_port; # introducer
-    }
+    bless $self, $class;
 
-    my $logfile = sprintf '%s-%s.log', $bind_host, $port;
-
-    unlink $logfile or die "Could not unlink $logfile: $!"
-        if -e $logfile;
-
-    push @cmd, '2>&1', '|', 'tee', $logfile;
-
-    my $cmd = join ' ', @cmd;
-
-    say "child: exec '$cmd', bye.";
-
-    exec $cmd;
+    return $self;
 }
 
-say "monitor: letting children run for ${test_runtime}s";
+do {
 
-sleep $test_runtime;
+    my %opts;
+    GetOptions( \%opts, 'ping_interval=i', 'test_runtime=i', 'phi_exec=s',
+        'log_root_dir=s', )
+      or die 'Invalid options found';
 
-say 'monitor: now terminating children';
+    __PACKAGE__->new( %opts, test_name => TEST_NAME )->main();
 
-kill 'TERM', @pids;
+} unless (caller);
 
-sub valid_ipv4_addr {
-    my @octets = split /\./, $_[0];
-    return all { looks_like_number($_) && $_ >= 0 && $_ <= 255 } @octets;
+sub main {
+    my ($self) = @_;
+
+    local $SIG{__DIE__} = sub {
+        my @args = @_;
+        $self->cleanup;
+        die @args;
+    };
+
+    $self->setup_logging();
+
+    my @cmds = (
+        [
+            $self->{phifd}, '-t', $self->{ping_interval}, '-a',
+            '127.0.0.1:12345'
+        ],
+        [
+            $self->{phifd},    '-t', $self->{ping_interval}, '-a',
+            '127.0.0.1:12346', '-i', '127.0.0.1:12345'
+        ],
+        [
+            $self->{phifd},    '-t', $self->{ping_interval}, '-a',
+            '127.0.0.1:12347', '-i', '127.0.0.1:12346'
+        ],
+    );
+
+    my @harnesses;
+    my @handles;
+    for my $procnum ( 0 .. $#cmds ) {
+        my $logfile = File::Spec->catfile( $self->test_log_dir,
+            'proc_' . $procnum . '.log' );
+
+        # XXX: do we need an :encoding(utf8) discipline here?
+        open my $fh, '>', $logfile
+          or die "Cannot open $logfile for writing: $!";
+
+        my $harness = harness $cmds[$procnum], \undef, $fh, $fh, init => sub {
+            $ENV{RUST_BACKTRACE} = 1;
+        };
+
+        push @handles,   $fh;
+        push @harnesses, $harness;
+    }
+
+    my $start = time;
+    while ( time - $start < $self->{test_runtime} ) {
+        $_->pump_nb for @harnesses;
+    }
+
+    $self->cleanup();
 }
+
+sub cleanup {
+    my ($self) = @_;
+    $_->kill_kill for @{ $self->{harnesses} };
+    close for @{ $self->{handles} };
+}
+
+sub setup_logging {
+    my ($self) = @_;
+    remove_tree( $self->test_log_dir );
+    make_path( $self->test_log_dir );
+}
+
+sub test_log_dir {
+    my ($self) = @_;
+    return File::Spec->catfile( $self->{log_root}, $self->{test_name} );
+}
+
+1;
